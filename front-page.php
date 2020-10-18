939113ffb9fdeb027bcac341e645d5b7d747f6ed
Margo <mnchek@mail.ru>
2020-10-18 15:36:35 +0300

    post 2

--- front-page.php
+++ front-page.php
@@ -164,7 +164,7 @@
                                         <span class="author-name"><strong><?php the_author(); ?></strong> : <?php the_author_meta( 'description' ) ?></span>
                                     </div>
                                     <div class="comments">
-                                        <img src="<?php echo get_template_directory_url() . './assets/images/comment.svg' ?>"
+                                        <img src="<?php echo get_template_directory_uri() . '/assets/images/comment.svg' ?>"
                                             alt="icon: comment" class="comments-icon">
                                         <span class="comments-counter"><?php comments_number('0', '1', '%'); ?></span>
                                     </div>
@@ -176,7 +176,7 @@
                     case '2': // выводим второй пост
                         ?>
                         <li class="article-grid-item article-grid-item-2">
-                            <!-- <img src="<?php //echo get_the_post_thumbnail_url(); ?>" alt="" class="article-grid-thumb"> -->
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="article-grid-thumb">
                             <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
                                 <span class="tag">
                                     <?php $posttags = get_the_tags(); // получаем все теги
@@ -191,15 +191,15 @@
                                         <?php $author_id = get_the_author_meta('ID'); ?>  <!-- получаем данные автора -->
                                         <img src="<?php echo get_avatar_url($author_id); ?>" alt="" class="author-avatar">
                                         <div class="author-info">
-                                            <span class="author-name"><strong><?php the_author(); ?></strong></span>
+                                            <span class="author-name"><?php the_author(); ?></span>
                                             <span class="date"><?php the_time( 'j F' ); ?></span>
                                             <div class="comments">
-                                                <img src="<?php echo get_template_directory_url() . './assets/images/comment-white.svg' ?>"
+                                                <img src="<?php echo get_template_directory_uri() . './assets/images/comment-white.svg' ?>"
                                                     alt="icon: comment" class="comments-icon">
                                                 <span class="comments-counter"><?php comments_number('0', '1', '%'); ?></span>
                                             </div>
                                             <div class="likes">
-                                                <img src="<?php echo get_template_directory_url() . './assets/images/heart.svg' ?>"
+                                                <img src="<?php echo get_template_directory_uri() . './assets/images/heart.svg' ?>"
                                                     alt="icon: like" class="likes-icon">
                                                 <span class="likes-counter"><?php comments_number('0', '1', '%'); ?></span>
                                             </div>
@@ -225,10 +225,9 @@
                     default: // выводим остальные посты
                         ?>
                         <li class="article-grid-item article-grid-item-default">
-                            <a href="<?php the_permalink(); ?>" class="article-grid-permalink">
-                                <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" class="article-thumb">       
+                            <a href="<?php the_permalink(); ?>" class="article-grid-permalink">      
                                 <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 56, '...'); ?></h4>
-                                <p class="article-grid-excerpt"><?php echo get_the_excerpt()?></p>
+                                <p class="article-grid-excerpt"><?php echo mb_strimwidth (get_the_excerpt(), 0, 120, '...'); ?></p>
                                 <span class="article-date"><?php the_time( 'j F' ); ?></span> 
                                 <!-- для вывода даты всех постов лучше использовать the_time( 'j F Y' );
                                 для вывода даты только у первого поста the_date( 'j F Y' ); -->
